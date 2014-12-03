886fe162525f085c3fddf4c434b803cb553c13f5
Connor <Cisco@10.22.127.109>
2014-12-03 09:47:44 -0500

    version 0.0.0.11 Pre Alpha -SpriteSheet created

--- src/com/wicgames/wicLibrary/SpriteSheet.java
+++ src/com/wicgames/wicLibrary/SpriteSheet.java
@@ -13,6 +13,7 @@ public class SpriteSheet {
 	private Vector2 spriteSize;
 	private Vector2 numSprites;
 	private BufferedImage sheet;
+	private BufferedImage[][] sprites;
 	public SpriteSheet(String path,int x,int y){
 		spriteSize = new Vector2(x,y);
 		try {
@@ -21,9 +22,11 @@ public class SpriteSheet {
 			e.printStackTrace();
 		}
 		numSprites = new Vector2(sheet.getWidth(null) / spriteSize.x, sheet.getHeight(null) / spriteSize.y);
+		sprites = new BufferedImage[x][y];
+		loadSprites();
 	}
-	public Image getSprite(int x, int y){
-		if(x > numSprites.x || y > numSprites.y);
+	private BufferedImage getSprite(int x, int y){
+		if(x > numSprites.x || y > numSprites.y) throw new ArrayIndexOutOfBoundsException();
 		Vector2 spritePosition = new Vector2(x * spriteSize.x,y * spriteSize.y);
 		BufferedImage sprite = (BufferedImage) Main.frame.createImage((int)spriteSize.x,(int)spriteSize.y);
 		for(int xPos = 0;xPos < spriteSize.x;xPos++)
@@ -32,4 +35,11 @@ public class SpriteSheet {
 			}
 		return sprite;
 	}
+	private void loadSprites(){
+		for(int x = 0;x < numSprites.x;x++)
+			for(int y = 0;y < numSprites.y;y++){
+				sprites[x][y] = getSprite(x,y);
+			}
+	}
+	public Image getImage(int x,int y){return sprites[x][y];}
 }
