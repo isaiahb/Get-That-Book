33f33d3fd34d3d770d52e336eef5882abc9e6061
Connor <connor.infernaldoom@gmail.com>
2014-12-05 18:33:27 -0500

    version 0.0.0.19 Pre Alpha -Stuff(IDK)

--- src/com/wicgames/game/OptionMenu.java
+++ src/com/wicgames/game/OptionMenu.java
@@ -2,17 +2,20 @@ package com.wicgames.game;
 
 import javax.swing.JComboBox;
 
+import com.wicgames.wicLibrary.Drawable;
 import com.wicgames.window.Button;
 import com.wicgames.window.Scene;
 
 public class OptionMenu extends Scene{
 	@Override
 	public void init() {
+		String[] res = {"1024x576","512x288","256x144"};
 		JComboBox<String> resolutions = new JComboBox<String>();
 		Button save = new Button("bin/assets/textures/SaveButton.png",(int)(Main.WIDTH * 0.75),(int)(Main.HEIGHT * 0.75));
 		save.addActionListener((e) ->{
 			
 		});
+		Main.panel.add(save);
+		currentScene.drawables.add((Drawable) save);
 	}
-	
 }
