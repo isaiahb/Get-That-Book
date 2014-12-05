da93487664801a4e0a560922a79f14cd6860e269
f1rebug <willhutchinson2.0@gmail.com>
2014-12-05 09:06:45 -0500

    version 0.0.0.19 Pre Alpha -Character started

--- src/com/wicgames/mobs/Character.java
+++ src/com/wicgames/mobs/Character.java
@@ -1,9 +1,18 @@
 package com.wicgames.mobs;
 
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
 public class Character extends Mob {
     public Character(){
         health = 100;
         armour = 10;
         damageBoost = 2;
+        try {
+            texture = ImageIO.read(new File("bin/assets/textures/character.png"));
+        } catch (IOException e) {
+            System.out.println("character texture failed");
+        }
     }
 }
--- src/com/wicgames/mobs/Mob.java
+++ src/com/wicgames/mobs/Mob.java
@@ -2,9 +2,10 @@ package com.wicgames.mobs;
 
 import com.wicgames.gameObjects.GameObject;
 
+import java.awt.*;
+
 public abstract class Mob extends GameObject{
     public int health;
     public int armour;
     public int damageBoost;
-
 }
