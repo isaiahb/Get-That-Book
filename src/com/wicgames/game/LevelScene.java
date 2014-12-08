d009ecfa35061dfc7de7af429a074dd8b7ffd3b7
isaiahb <isaiahballah@gmail.com>
2014-12-08 09:04:57 -0500

    version 0.0.0.42 Pre Alpha -Finally

--- src/com/wicgames/game/LevelScene.java
+++ src/com/wicgames/game/LevelScene.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import com.sun.org.apache.bcel.internal.generic.GOTO;
 import com.wicgames.gameObjects.GameObject;
 import com.wicgames.gameObjects.InvisiblePlatform;
 import com.wicgames.gameObjects.Platform;
@@ -13,6 +14,7 @@ import com.wicgames.gameObjects.Scenery;
 import com.wicgames.mobs.Character;
 import com.wicgames.physics.Force;
 import com.wicgames.physics.Material;
+import com.wicgames.physics.Rectangle;
 import com.wicgames.wicLibrary.SpriteSheet;
 import com.wicgames.window.Scene;
 public class LevelScene extends Scene {
@@ -46,25 +48,23 @@ public class LevelScene extends Scene {
 	 * @param path Location of level file
 	 */
 	public void loadLevel(String path){
+		int levelNumber = Integer.parseInt(levelName.substring(5, levelName.length()));
+		System.out.println(levelNumber);
 		try {
 			int x = 0,y = 0; //X,Y of tile
 			BufferedReader levelReader = new BufferedReader(new FileReader(path));
 			textureData = new Data(path + "text");
 			textures = new SpriteSheet(textureData.getValue("spritesheet"), Integer.parseInt(textureData.getValue("x")), Integer.parseInt(textureData.getValue("y")), Integer.parseInt(textureData.getValue("border")),0);
+			LevelReader:
 			while(levelReader.ready()){
 				x = 0; //New Row set x to 0
 				char[] levelLine = levelReader.readLine().toCharArray();
 				for(char tile : levelLine) {
 					
-					if(tile == '&')break; //& is character for end of level background
+					if(tile == '&')break LevelReader; //& is character for end of level background
 					if(String.valueOf(tile).equals(textureData.getValue("sky"))){x++; continue;}
-					GameObject obj;
-					if (!String.valueOf(tile).equals(textureData.getValue("invisible"))) 
-							obj = tile < 64? 
-									new Platform(x,y,1,1,textures.getImage(Integer.parseInt(textureData.getValue(String.valueOf(tile))))):					
-									new Scenery(x,y,1,1,textures.getImage(Integer.parseInt(textureData.getValue(String.valueOf(tile)))));
-					else 
-						obj = new InvisiblePlatform(x,y,1,1);		
+					GameObject obj;		
+					obj = new Scenery(x,y,1,1,textures.getImage(Integer.parseInt(textureData.getValue(String.valueOf(tile)))));
 					obj.body.setMaterial(Material.Static);
 					size.x = Math.max(x * GameObject.tileSize, size.x);
 					size.y = Math.max(y * GameObject.tileSize, size.y);
@@ -76,6 +76,15 @@ public class LevelScene extends Scene {
 				
 			}
 			levelReader.close();
+			BufferedReader hitboxReader = new BufferedReader(new FileReader(path + "hitmap"));//TODO wont work above level ten
+			while(hitboxReader.ready()){
+				String[] rectangle = hitboxReader.readLine().split(" ");
+				Rectangle hitbox = new Rectangle(Integer.parseInt(rectangle[0]) * GameObject.tileSize,Integer.parseInt(rectangle[1]) * GameObject.tileSize,Integer.parseInt(rectangle[2]) * GameObject.tileSize,Integer.parseInt(rectangle[3]) * GameObject.tileSize);
+				Scene.currentScene.bodies.add(hitbox);
+				hitbox.setMaterial(Material.Static);
+				
+			}
+			hitboxReader.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
